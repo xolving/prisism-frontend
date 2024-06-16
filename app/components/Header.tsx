491b94eecedc4b4b4657a07e4508231aba30f306
enbraining <me@xolving.com>
2024-06-16 15:33:43 +0900

    :recycle: 모바일에선 헤더를 표시하지 않음

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -10,17 +10,33 @@ const StyledHeader = styled.header`
     padding: 18px 50px 18px 50px;
     border-bottom: 1px;
     border-bottom: 1px solid #2f2f2f;
+
+    @media screen and (max-width: 768px) {
+        display: none;
+    }
+`
+
+const Menu = styled.span`
+    display: flex;
+    column-gap: 2rem;
+    justify-content: center;
+    align-items: center;
+    margin-left: 3rem;
+    margin-right: 3rem;
+    font-size: 1.125rem;
+    line-height: 1.75rem;
+    font-weight: 500;
 `
 
 export default function Header(){
     return (
         <StyledHeader>
             <Link href={"/"}><Logo width={50} height={50} /></Link>
-            <span className="flex gap-x-8 justify-center items-center mx-12 text-lg font-medium">
+            <Menu>
                 <Link href={"/"}>공지사항</Link>
                 <Link href={"/"}>문의하기</Link>
                 <Link href={"/"}>게시판</Link>
-            </span>
+            </Menu>
         </StyledHeader>
     )
 }

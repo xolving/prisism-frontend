8dc164ad34a9c215e3b0451ca3a27b4a150331ca
enbraining <me@xolving.com>
2024-06-17 08:35:49 +0900

    :recycle: SWC

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
+    swcMinify: true,
     compiler: {
       styledComponents: true,
-    },
+    }
 };
 
 export default nextConfig;

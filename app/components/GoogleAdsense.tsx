6a2876ca97165afd855e477d356b39c2bd40f3f8
enbraining <me@xolving.com>
2024-06-17 10:14:49 +0900

    :feature: 구글 애드센스 추가

---
+++ app/components/GoogleAdsense.tsx
@@ -0,0 +1,11 @@
+export default function GoogleAdsense(){
+    if (process.env.NODE_ENV !== "production") {
+        return null;
+    }
+    return (
+        <script
+            async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5938651528318065"
+            crossOrigin="anonymous">
+        </script>
+      );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from 'next/font/local';
+import GoogleAdsense from "./components/GoogleAdsense";
 import Header from "./components/Header";
 import "./globals.css";
 
@@ -23,6 +24,7 @@ export default function RootLayout({
         <Header />
         {children}
       </body>
+      <GoogleAdsense />
     </html>
   );
 }

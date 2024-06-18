92192778b8602ea9e85bd027d82f923af86886af
enbraining <me@xolving.com>
2024-06-18 22:20:48 +0900

    :recycle: update about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function page(){
     return (
         <main className="p-20 grid gap-y-10">
@@ -6,9 +8,9 @@ export default function page(){
                 <p>프리시즘은 채팅을 저장하지 않으며 또한 아무런 정보도 수집하지 않습니다.</p>
             </div>
             <div>
-                <h1 className="text-3xl pb-2 font-medium">후원하기</h1>
-                <p>후원하신 돈은 서버 관리를 위해서 사용됩니다.</p>
-                <p>신한 110 553 243459</p>
+                <h1 className="text-3xl pb-2 font-medium">디스코드</h1>
+                <p>프리시즘에 버그가 있거나 건의사항이 있다면 디스코드 서버에 올려주세요.</p>
+                <Link className=" text-blue-500" href={"https://discord.gg/FuWPb5pP"}>https://discord.gg/FuWPb5pP</Link>
             </div>
         </main>
     )

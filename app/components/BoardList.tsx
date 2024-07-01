0e49edf40a4c67f2ac83d918594e04d2444fa832
enbraining <me@fodo.dev>
2024-07-02 01:08:48 +0900

    :recycle: 예시 이미지 추가

--- app/components/BoardList.tsx
+++ app/components/BoardList.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import styled from 'styled-components'
 import { Board } from '../types/Board'
 import { toKoreanDate } from '../util/korean-date'
@@ -5,18 +6,22 @@ import { toKoreanDate } from '../util/korean-date'
 const StyledContent = styled.a`
   border: 1px solid var(--background-secondary-rgb);
   border-radius: 15px;
-  height: 150px;
-  padding: 20px;
+  padding: 10px;
   background-color: #262626;
+  display: flex;
+  column-gap: 10px;
 `
 
 export default function BoardList({ boardList }: { boardList: Board[] }) {
   return (
-    <div className="grid mt-10 grid-cols-3 gap-4">
+    <div className="grid mt-10 grid-cols-2 gap-4">
       {boardList?.map((board, index) => (
         <StyledContent key={index} href={`/board/${board.id}`}>
-          <p className="ml-auto">{`${toKoreanDate(board.createdAt)}`}</p>
-          <p className="text-lg">{board.title}</p>
+          <Image src={'/example.png'} alt="example" width={200} height={200} className="rounded-xl" />
+          <div className="p-3">
+            <p className="ml-auto">{`${toKoreanDate(board.createdAt)}`}</p>
+            <p className="text-lg">{board.title}</p>
+          </div>
         </StyledContent>
       ))}
     </div>
